3e5c95b8a5fd927bf6ec34113418eec18aa1569d
Alex Martínez <martinez246alex@gmail.com>
2024-06-07 06:48:34 -0500

    feat(global): configure jwt authentication filter and jwt service
    adding pom xml dependencies

--- src/main/java/com/security/spring_security/Auth/AuthService.java
+++ src/main/java/com/security/spring_security/Auth/AuthService.java
@@ -22,7 +22,7 @@ public class AuthService {
 
     public AuthResponse login(LoginRequest loginRequest) {
         authenticationManager.authenticate(new UsernamePasswordAuthenticationToken(loginRequest.getUsername(),loginRequest.getPassword()));
-        UserDetails user = userRepository.findByUsername(loginRequest.getUsername()).orElseThrow();
+        User user = userRepository.findByUsername(loginRequest.getUsername()).orElseThrow();
         String token = jwtService.getToken(user);
         return AuthResponse.builder()
         .token(token)
--- src/main/java/com/security/spring_security/Jwt/JwtService.java
+++ src/main/java/com/security/spring_security/Jwt/JwtService.java
@@ -1,38 +1,77 @@
 package com.security.spring_security.Jwt;
 
+import com.security.spring_security.User.User;
+import io.jsonwebtoken.Claims;
 import io.jsonwebtoken.Jwts;
 import io.jsonwebtoken.SignatureAlgorithm;
 import io.jsonwebtoken.io.Decoders;
 import io.jsonwebtoken.security.Keys;
+import org.springframework.beans.factory.annotation.Value;
 import org.springframework.security.core.userdetails.UserDetails;
 import org.springframework.stereotype.Service;
 
-import java.security.Key;
+import javax.crypto.SecretKey;
 import java.util.Date;
 import java.util.HashMap;
 import java.util.Map;
+import java.util.function.Function;
 
 @Service
 public class JwtService {
 
-    private static final String SECRET_KEY = "586ED561FSD324235AS15ASD35DA1S3132SAD15AS15D65";
+    @Value("${jwt.secret}")
+    private String SECRET_KEY;
 
-    public String getToken(UserDetails user) {
+    public String getToken(User user) {
         return getToken(new HashMap<>(),user);
     }
 
-    private String getToken(Map <String, Object> extraClaims, UserDetails user) {
+    private String getToken(Map <String, Object> extraClaims, User user) {
         return Jwts.builder()
-                .setClaims(extraClaims)
-                .setSubject(user.getUsername())
-                .setIssuedAt(new Date(System.currentTimeMillis()))
-                .setExpiration(new Date(System.currentTimeMillis()+1000*60*24))
-                .signWith(getKey(), SignatureAlgorithm.HS256)
+                .claims(extraClaims)
+                .claim("userId",user.getId())
+                .claim("userName",user.getUsername())
+                .subject(user.getUsername())
+                .issuedAt(new Date(System.currentTimeMillis()))
+                .expiration(new Date(System.currentTimeMillis()+1000*60*24))
+                .signWith(getKey())
                 .compact();
     }
 
-    private Key getKey() {
+    private SecretKey getKey() {
         byte[] keyBytes = Decoders.BASE64.decode(SECRET_KEY);
         return Keys.hmacShaKeyFor(keyBytes);
+    }
+
+    public String getUsernameFromToken(String token) {
+        return getClaim(token,Claims::getSubject);
+    }
+
+    public boolean isTokenValid(String token, UserDetails userDetails) {
+        final String username = getUsernameFromToken(token);
+        return (username.equals(userDetails.getUsername())&& !isTokenExpired(token));
+
+    }
+
+    private Claims getAllClaims (String token){
+        return Jwts.
+                parser()
+                .verifyWith(getKey())
+                .build()
+                .parseSignedClaims(token)
+                .getPayload();
+    }
+
+    public <T> T getClaim(String token, Function<Claims,T> claimsResolver){
+        final Claims claims = getAllClaims(token);
+        return claimsResolver.apply(claims);
+    }
+
+    private Date getExpiration(String token){
+        return  getClaim(token,Claims::getExpiration);
+    }
+
+    private boolean isTokenExpired(String token){
+        return getExpiration(token).before(new Date());
     }
 }
